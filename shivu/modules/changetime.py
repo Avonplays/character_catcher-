6ab95b8b1d3e64ca7a797d8837e86f463b773b88
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-25 22:35:30 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -23,8 +23,8 @@ async def change_time(update: Update, context: CallbackContext) -> None:
 
         
         new_frequency = int(args[0])
-        if new_frequency < 10:
-            await update.message.reply_text('The message frequency must be greater than or equal to 10.')
+        if new_frequency < 25:
+            await update.message.reply_text('The message frequency must be greater than or equal to 25.')
             return
 
         
